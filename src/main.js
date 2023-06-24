08c7b63c33f35c2ff600b321561220acb1d028e3
GrasonMC <90360132+Grant-JHP@users.noreply.github.com>
2023-06-24 10:17:32 +1000

    initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+document.querySelector('div#app').innerHTML = `
+  <h1>Hello World!</h1>
+`;
+
+console.log('Hello World!');
---
+++ vite.config.js
@@ -0,0 +1,5 @@
+export default {
+    root: './src',
+    publicDir: '../public',
+    build: { outDir: '../dist' }
+}
